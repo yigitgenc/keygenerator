bef7792bc0ea3dacb4a344c027545a1830f81796
Yigit Genc <yigidix@gmail.com>
2012-07-25 15:14:42 +0300

    fixed length issue.

--- keygenerator.py
+++ keygenerator.py
@@ -41,7 +41,7 @@ class KeyGenerator(object):
 		self.filename 	= str(filename)
 
 	def __generateKey(self):
-		self.alphabet 	= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X' 'Y', 'Z']
+		self.alphabet 	= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 		self.digits 	= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 		self.alnumlib 	= self.alphabet + self.digits
 
